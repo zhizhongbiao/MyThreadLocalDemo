7b6ae6e37ac81a54121783f9f149164029a494e4
zhizhongbiao <zhizhongbiao163@163.com>
2019-09-09 19:42:12 +0800

    practice

--- app/src/main/java/com/waterflower/mythreadlocaldemo/my_thread_local/MyThreadLocalImp.java
+++ app/src/main/java/com/waterflower/mythreadlocaldemo/my_thread_local/MyThreadLocalImp.java
@@ -1,9 +1,6 @@
 package com.waterflower.mythreadlocaldemo.my_thread_local;
 
-import android.os.HandlerThread;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.ref.WeakReference;
 
 /**
  * FileName :  MyThreadLocalImp
@@ -35,9 +32,9 @@ public class MyThreadLocalImp<T> implements IMyThreadLocal<T> {
         Thread currentThread = Thread.currentThread();
         MyThreadLocalMap map = getMap(currentThread);
         if (map != null) {
-            MyThreadLocalMap.Entry e = map.getEntry(this);
+            MyThreadLocalMap.MyWeakReferenceEntry e = map.getEntry(this);
             if (e != null) {
-                return (T) e.getValue();
+                return (T) e.value;
             }
         }
         return setInitValue();
@@ -58,7 +55,7 @@ public class MyThreadLocalImp<T> implements IMyThreadLocal<T> {
     }
 
     private void createMap(Thread currentThread, T v) {
-        ((MyThread) currentThread).threadLocalMap=new MyThreadLocalMap(currentThread,v);
+        ((MyThread) currentThread).threadLocalMap = new MyThreadLocalMap(this, v);
     }
 
     private MyThreadLocalMap getMap(Thread currentThread) {
@@ -66,48 +63,125 @@ public class MyThreadLocalImp<T> implements IMyThreadLocal<T> {
     }
 
 
-    public class MyThreadLocalMap {
-        private Thread t;
+    static class MyThreadLocalMap {
 
-        public MyThreadLocalMap(Thread t, Object firstValue) {
-            this.t = t;
+
+        /**
+         * Table's initial capacity.Capacity Must be power of 2.
+         * Table.length Must be a power of 2.
+         */
+        private int INITIAL_CAPACITY = 16;
+
+        /**
+         * Table 's size;
+         */
+        private int size = 0;
+
+
+        /**
+         * A threshold for that table.
+         */
+        private int threshold = 0;
+
+        /**
+         * An array for MyWeakReferenceEntry.
+         */
+        MyWeakReferenceEntry[] table;
+
+
+        public MyThreadLocalMap(MyThreadLocalImp firstKey, Object firstValue) {
+            table = new MyWeakReferenceEntry[INITIAL_CAPACITY];
+            int i = firstKey.hashCode() & (INITIAL_CAPACITY - 1);
+            table[i] = new MyWeakReferenceEntry(firstKey, firstValue);
+            size = 1;
+            setThreshold(INITIAL_CAPACITY);
+        }
+
+        private void setThreshold(int thresholdCapacity) {
+            threshold = thresholdCapacity;
         }
 
 
-        public void set(IMyThreadLocal key, T v) {
+        public void set(IMyThreadLocal key, Object v) {
+            MyWeakReferenceEntry[] entries = table;
+            int len = entries.length;
+            int i = key.hashCode() & (len - 1);
 
+
+            for (MyWeakReferenceEntry e = entries[i];
+                 e != null;
+                 e = entries[getNextIndex(i, len)]) {
+
+                IMyThreadLocal iMyThreadLocal = e.get();
+                if (key == iMyThreadLocal) {
+                    e.value = v;
+                    return;
+                }
+
+                if (iMyThreadLocal == null) {
+                    //Do somethings necessary.
+                    return;
+                }
+            }
+
+            table[i] = new MyWeakReferenceEntry(key, v);
+            size++;
+            //Do things left.
         }
 
 
-        public T get() {
+        public MyWeakReferenceEntry getEntry(MyThreadLocalImp key) {
+
+            int i = key.hashCode() & (table.length - 1);
+            MyWeakReferenceEntry e = table[i];
+
+            if (e != null && e.get() == key) {
+                return e;
+            }
+            return getEntryAfterMiss(key, i, e);
+
+
+        }
+
+        private MyWeakReferenceEntry getEntryAfterMiss(MyThreadLocalImp key, int index, MyWeakReferenceEntry e) {
+
+            MyWeakReferenceEntry[] tab = table;
+            int len = tab.length;
+
+            while (e != null) {
+                IMyThreadLocal k = e.get();
+                if (k == key) {
+                    return e;
+                } else if (k == null) {
+                    //Do other necessary things.
+                } else {
+                    index = getNextIndex(index, len);
+                }
+
+                e = table[index];
+            }
+
+
             return null;
+
         }
 
+        private int getNextIndex(int i, int len) {
+            return 0;
+        }
 
         public void remove(IMyThreadLocal key) {
 
         }
 
-        public MyThreadLocalMap.Entry getEntry(MyThreadLocalImp<T> tMyThreadLocalImp) {
-            return null;
-        }
 
+        static class MyWeakReferenceEntry extends WeakReference<IMyThreadLocal> {
 
-        private class Entry implements Map.Entry<IMyThreadLocal, Object> {
+            private Object value;
 
-            @Override
-            public IMyThreadLocal getKey() {
-                return null;
-            }
-
-            @Override
-            public Object getValue() {
-                return null;
-            }
-
-            @Override
-            public Object setValue(Object value) {
-                return null;
+            public MyWeakReferenceEntry(IMyThreadLocal key, Object v) {
+                super(key);
+                this.value = v;
             }
         }
     }
